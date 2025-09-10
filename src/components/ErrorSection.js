10db893a9ee25747c4ee2c12587217064cd6e46d
rednamed <estou.eu.te.amo@gmail.com>
2025-09-10 22:03:04 +0900

    chore: 手動編集内容と不要jsファイル削除を反映

--- src/components/ErrorSection.js
+++
@@ -1,4 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-export default function ErrorSection({ message }) {
-    return (_jsx("div", { className: "bg-red-50 border border-red-200 rounded-xl p-6 mb-8", children: _jsxs("div", { className: "flex items-center", children: [_jsx("div", { className: "flex-shrink-0", children: _jsx("svg", { className: "h-5 w-5 text-red-400", viewBox: "0 0 20 20", fill: "currentColor", children: _jsx("path", { fillRule: "evenodd", d: "M10 18a8 8 0 100-16 8 8 0 000 16zM8.707 7.293a1 1 0 00-1.414 1.414L8.586 10l-1.293 1.293a1 1 0 101.414 1.414L10 11.414l1.293 1.293a1 1 0 001.414-1.414L11.414 10l1.293-1.293a1 1 0 00-1.414-1.414L10 8.586 8.707 7.293z", clipRule: "evenodd" }) }) }), _jsx("div", { className: "ml-3", children: _jsx("p", { className: "text-sm text-red-700 font-medium", children: message }) })] }) }));
-}
